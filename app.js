c0159f76b57e050227a3cac632ac9012ec03e6e7
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-09-20 08:41:59 +0700

    Implement getting list of routes

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var echo = require('./routes/echo');
-
 var app = express();
 
 // view engine setup
@@ -26,8 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/echo', echo);
+// app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

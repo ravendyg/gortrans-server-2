1377d89498eb4cb76a1a1ae04ffbf4fa5b32af3c
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-07-28 21:24:22 +0700

    Temp disable socket verification

--- src/lib/services/socket.js
+++ src/lib/services/socket.js
@@ -28,7 +28,7 @@ function start(server)
   io.use(
     (socket, next) =>
     {
-      let apiKey = +socket.handshake.query.api_key;
+      let apiKey = +socket.handshake.query.api_key || -1;
       let ip = socket.handshake.headers['x-real-ip'];
       let agent = socket.handshake.headers['user-agent'];
       if (!apiKey)
